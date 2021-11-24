7ee846792ebabb751754c939a3329f196f9f78ed
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2021-11-24 14:40:31 +0200

    refactoring

--- Game.cs
+++ Game.cs
@@ -44,51 +44,16 @@ namespace EternityRPG
             { 4, "run away" }
         };
 
-        public static void InitializeWorld()
+        public static void InitializeWorld(int worldSize = 4)
         {
-            bosses = new Enemy[]
+            bosses = new Enemy[worldSize];
+            biomes = new Biome[worldSize];
+
+            for (int i = 0; i < worldSize; i++)
             {
-                new Boss(bossType: 1),
-                new Boss(bossType: 2),
-                new Boss(bossType: 3),
-                new Boss(bossType: 4)
-            };
-
-            biomes = new Biome[]
-            {
-                new Biome(biomeType: 1),
-                new Biome(biomeType: 2),
-                new Biome(biomeType: 3),
-                new Biome(biomeType: 4)
-            };
-
-            inventory = new Item[]
-            {
-                new Potion(potionType: 1),
-                new Potion(potionType: 2),
-                new Potion(potionType: 3),
-                new Weapon(player.Class, weaponType: 1),
-                new Weapon(player.Class, weaponType: 2)
-            };
-        }
-
-        /*
-        public static void InitializeWorld(bool hardMode)
-        {
-            int sizeOfWorld;
-            HardMode = hardMode;
-
-            if (HardMode) 
-                sizeOfWorld = 6;
-            else sizeOfWorld = 4;
-
-            bosses = new Enemy[sizeOfWorld];
-            for (int i = 0; i < bosses.Length; i++)
                 bosses[i] = new Boss(bossType: i + 1);
-
-            biomes = new Biome[sizeOfWorld];
-            for (int i = 0; i < bosses.Length; i++)
                 biomes[i] = new Biome(biomeType: i + 1);
+            }
 
             inventory = new Item[]
             {
@@ -99,7 +64,6 @@ namespace EternityRPG
                 new Weapon(player.Class, weaponType: 2)
             };
         }
-         */
 
         public static int AmountOfPotions()
         {
